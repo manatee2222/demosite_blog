85be6fb95505494f268ca475c7b711555daf7b05
manatee2222 <63432673+manatee2222@users.noreply.github.com>
2022-02-06 14:54:21 +0900

    Update function.js

--- function.js
+++ function.js
@@ -1,7 +1,19 @@
-$('.slider').slick({
+$(function(){
+  // 「#login-show」要素に対するclickイベントを作成してください
+  $('#menu-show').click(function(){
+    $('#header_menu').fadeIn();
+  });
+
+  $('.close-modal').click(function(){
+    $('#header_menu').fadeOut();
+    
+  });
+
+
+  $('.slider').slick({
     autoplay: true,//自動的に動き出すか。初期値はfalse。
     autoplaySpeed: 3000,//次のスライドに切り替わる待ち時間
-    speed:2000,//スライドの動きのスピード。初期値は300。
+    speed:1000,//スライドの動きのスピード。初期値は300。
     infinite: true,//スライドをループさせるかどうか。初期値はtrue。
     slidesToShow: 1,//スライドを画面に3枚見せる
     slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
@@ -13,27 +25,11 @@ $('.slider').slick({
         pauseOnHover: false,//マウスホバーで一時停止を無効
         pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
     });
-
-//スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
-$('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
+  
+  //スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
+  $('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
     $('.slider').slick('slickPlay');
+  });
+
+
 });
-
-
-//任意のタブにURLからリンクするための設定
-function GethashID (hashIDName){
-    if(hashIDName){
-      //タブ設定
-      $('.tab li').find('a').each(function() { //タブ内のaタグ全てを取得
-        var idName = $(this).attr('href'); //タブ内のaタグのリンク名（例）#lunchの値を取得 
-        if(idName == hashIDName){ //リンク元の指定されたURLのハッシュタグ（例）http://example.com/#lunch←この#の値とタブ内のリンク名（例）#lunchが同じかをチェック
-          var parentElm = $(this).parent(); //タブ内のaタグの親要素（li）を取得
-          $('.tab li').removeClass("active"); //タブ内のliについているactiveクラスを取り除き
-          $(parentElm).addClass("active"); //リンク元の指定されたURLのハッシュタグとタブ内のリンク名が同じであれば、liにactiveクラスを追加
-          //表示させるエリア設定
-          $(".area").removeClass("is-active"); //もともとついているis-activeクラスを取り除き
-          $(hashIDName).addClass("is-active"); //表示させたいエリアのタブリンク名をクリックしたら、表示エリアにis-activeクラスを追加 
-        }
-      });
-    }
-  }
